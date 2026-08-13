cc218283e51a5e169255a585ba5aee12e4cbfa35
coldwind444 <minhtan14042004@gmail.com>
2026-08-13 13:48:11 +0700

    add /register to public urls

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-const PUBLIC_ROUTES = ["/login"];
+const PUBLIC_ROUTES = ["/login", "/register"];
 const URL_MAP = new Map<string, string>([
     ["ADMIN", "/admin/dashboard"],
     ["SERVER", "/server/tables"],
